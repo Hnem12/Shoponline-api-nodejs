a52a809ca4494f0ef0f642693aa22c9a844ebb27
Hnem <149763377+Hnem12@users.noreply.github.com>
2024-01-04 20:14:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 var bodyParser = require('body-parser')
 const AccountModel = require('./models/account')
 const DanhmucModel = require('./models/danhmuc')
 const SanphamModel= require('./models/sanpham')
 
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-var accountRouter= require('./routers/account')
+
+var router = require('./router/apiRouter')
 app.use('/api/account/', accountRouter)
 
-var danhmuctRouter= require('./routers/danhmuc')
+var danhmuctRouter= require('./router/danhmuc')
 app.use('/api/danhmuc/', danhmuctRouter)
 
-var sanphamRouter= require('./routers/sanpham')
+var sanphamRouter= require('./router/sanpham')
 app.use('/api/sanpham/', sanphamRouter)
 
 
-app.get('/', (req, res, next) => {
-    res.json('HOME') 
+
+app.get('/', (req, res, next) =>{   
+    res.json("Home")
 })
-
-
-app.listen(3000, () => {
-    console.log(`Server started on port`);
+app.listen(5000,() =>{
+    console.log(`Link: http://localhost:${5000}`)
 })
